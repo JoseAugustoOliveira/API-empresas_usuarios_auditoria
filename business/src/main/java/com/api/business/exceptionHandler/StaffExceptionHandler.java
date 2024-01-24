017448ffcf010161b5f7d098c382f5a2f8d064b2
Jose Augusto de Oliveira Pins <joliveirapins.jaop@gmail.com>
2024-01-24 10:10:15 -0300

    Ajustes no handler exception

--- business/src/main/java/com/api/business/exceptionHandler/StaffExceptionHandler.java
+++ business/src/main/java/com/api/business/exceptionHandler/StaffExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.api.business.exceptionHandler;
 
 import com.api.business.exceptions.CompanyErrorException;
+import com.api.business.exceptions.CompanyNotFoundException;
+import com.api.business.exceptions.EmptyListCompaniesException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,7 +16,24 @@ public class StaffExceptionHandler {
     @ExceptionHandler(CompanyErrorException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<String> handleCompanyErrorException(CompanyErrorException ex) {
-        String errorMessage = "Error registering company: " + ex.getMessage();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EmptyListCompaniesException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleEmptyListCompaniesException(EmptyListCompaniesException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(CompanyNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleCompanyNotFoundException(CompanyNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
